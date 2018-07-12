c37a00b64b7e2487966ae07c5b94a9275d049669
uhdang <uhdang@gmail.com>
2018-07-12 19:04:07 +0200

    meh

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,11 +49,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
-
+    'rest_framework',
 )
 
 LOCAL_APPS = (
-
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -152,4 +152,5 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
 
-
+REST_FRAMEWORK = {
+}
